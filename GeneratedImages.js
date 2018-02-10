081f7e7b426375a44933bd20fd9a7031530cea93
Daniel Hackner <dan.hackner@gmail.com>
2018-02-10 11:19:26 -0800

    Fix Magnesium Sulfate image reference

--- GeneratedImages.js
+++ GeneratedImages.js
@@ -35,6 +35,7 @@ const requiredImages = {
 ,  './img/Adrenaline.jpg': require('./img/Adrenaline.jpg')
 ,  './img/Diazepam.jpg': require('./img/Diazepam.jpg')
 ,  './img/Glucose.jpg': require('./img/Glucose.jpg')
+,  './img/MagnesiumSulfate.jpg': require('./img/MagnesiumSulfate.jpg')
 ,  './img/Naloxone.jpg': require('./img/Naloxone.jpg')
 ,  './img/Naloxone.jpg': require('./img/Naloxone.jpg')
 ,  './img/Salbutamol.jpg': require('./img/Salbutamol.jpg')
